51a94a834ce00811b70eb90f94dbcf27f4377cee
RaafaRodrigues <rafa.oliiver96@gmail.com>
2023-12-13 20:58:52 -0300

    properties, Game and ORM.

---
+++ src/main/java/com/curso/spring/boot/entities/Game.java
@@ -0,0 +1,43 @@
+package com.curso.spring.boot.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_game")
+@EqualsAndHashCode(of = "id")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "platform")
+    private String platform;
+
+    @Column(name = "score")
+    private Double score;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "short_description")
+    private String shortDescription;
+
+    @Column(name = "long_description")
+    private String longDescription;
+}
---
+++ src/main/java/com/curso/spring/boot/entities/GameList.java
@@ -0,0 +1,19 @@
+package com.curso.spring.boot.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+//import java.util.List;
+//
+//@Data
+//@Entity(name = "")
+//public class GameList {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//
+//    private List<Game> gameList;
+//
+//}
